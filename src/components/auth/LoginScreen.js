c754ca63dd856709d1961fae6ceae44d243ec7b7
David Portilla <david_portilla@hotmail.com>
2022-03-31 16:54:19 -0500

    adding redux and forms custom hooks

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import {useForm} from "../../hooks/useForm";
 
 export const LoginScreen = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        email: '',
+        password: '123456'
+    })
+
+    const {email, password} = formValues;
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
-
             <form>
-
                 <input
                     type="text"
                     placeholder="Email"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -21,8 +30,9 @@ export const LoginScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value={password}
+                    onChange={handleInputChange}
                 />
-
 
                 <button
                     type="submit"
@@ -31,13 +41,10 @@ export const LoginScreen = () => {
                     Login
                 </button>
 
-
                 <div className="auth__social-networks">
                     <p>Login with social networks</p>
 
-                    <div
-                        className="google-btn"
-                    >
+                    <div className="google-btn">
                         <div className="google-icon-wrapper">
                             <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
                         </div>
