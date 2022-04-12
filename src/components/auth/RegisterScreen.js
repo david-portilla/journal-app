736ea0c29543f7e702253c56abc93af5030264f6
David Portilla <david_portilla@hotmail.com>
2022-04-12 16:54:22 -0500

    adding validation feedback

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import validator from 'validator'
 import {removeError, setError} from '../../actions/ui';
@@ -8,6 +8,8 @@ import {useForm} from '../../hooks/useForm';
 export const RegisterScreen = () => {
 
     const dispatch = useDispatch();
+
+    const {msgError} = useSelector(state => state.ui);
 
     const [formValues, handleInputChange] = useForm({
         name: 'David',
@@ -47,7 +49,10 @@ export const RegisterScreen = () => {
 
             <form onSubmit={handleRegister}>
 
-                <div className='auth__alert-error'> Form error </div>
+                {
+                    msgError &&
+                    <div className='auth__alert-error'> {msgError} </div>
+                }
 
                 <input
                     type="text"
